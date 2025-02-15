d43645e4be73def3f37d2f85b5e56d12b8a6e4f6
priyanka462002 <lspriyanka04@gmail.com>
2025-02-15 17:39:11 +0530

    Adding Product Entity

---
+++ src/main/java/com/example/git/model/Product.java
@@ -0,0 +1,31 @@
+package com.example.git.model;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private int price;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
